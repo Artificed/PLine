8e066e99b1bc478be2d15cffaaad92fefdbb64f4
artificed <paultsai.id@gmail.com>
2024-12-11 16:44:42 +0700

    feat: Added chat textbox

---
+++ src/components/ChatTextBox.tsx
@@ -0,0 +1,60 @@
+const ChatTextBox: React.FC = () => {
+  return (
+    <div className="flex flex-col h-[7.6rem] justify-between border-t border-white/10 text-xs px-3 py-2 text-white font-extralight">
+      <div className="h-5/6 mb-2 mt-1">
+        <textarea
+          placeholder="Enter a message"
+          className="bg-transparent focus:outline-none w-full h-full text-start resize-none"
+        ></textarea>
+      </div>
+      <div className="flex justify-between">
+        <div className="flex items-center mb-1 ml-1 gap-2.5">
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            fill="none"
+            viewBox="0 0 24 24"
+            strokeWidth={1.5}
+            stroke="gray"
+            className="size-5"
+          >
+            <path
+              strokeLinecap="round"
+              strokeLinejoin="round"
+              d="m18.375 12.739-7.693 7.693a4.5 4.5 0 0 1-6.364-6.364l10.94-10.94A3 3 0 1 1 19.5 7.372L8.552 18.32m.009-.01-.01.01m5.699-9.941-7.81 7.81a1.5 1.5 0 0 0 2.112 2.13"
+            />
+          </svg>
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            fill="none"
+            viewBox="0 0 24 24"
+            strokeWidth={1.5}
+            stroke="gray"
+            className="size-5"
+          >
+            <path
+              strokeLinecap="round"
+              strokeLinejoin="round"
+              d="M17.593 3.322c1.1.128 1.907 1.077 1.907 2.185V21L12 17.25 4.5 21V5.507c0-1.108.806-2.057 1.907-2.185a48.507 48.507 0 0 1 11.186 0Z"
+            />
+          </svg>
+        </div>
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          fill="none"
+          viewBox="0 0 24 24"
+          strokeWidth={1.5}
+          stroke="gray"
+          className="size-5"
+        >
+          <path
+            strokeLinecap="round"
+            strokeLinejoin="round"
+            d="M15.182 15.182a4.5 4.5 0 0 1-6.364 0M21 12a9 9 0 1 1-18 0 9 9 0 0 1 18 0ZM9.75 9.75c0 .414-.168.75-.375.75S9 10.164 9 9.75 9.168 9 9.375 9s.375.336.375.75Zm-.375 0h.008v.015h-.008V9.75Zm5.625 0c0 .414-.168.75-.375.75s-.375-.336-.375-.75.168-.75.375-.75.375.336.375.75Zm-.375 0h.008v.015h-.008V9.75Z"
+          />
+        </svg>
+      </div>
+    </div>
+  );
+};
+
+export default ChatTextBox;
--- src/layouts/chat/ChatContent.tsx
+++ src/layouts/chat/ChatContent.tsx
@@ -1,9 +1,12 @@
 import ChatHeader from "../../components/ChatHeader";
+import ChatTextBox from "../../components/ChatTextBox";
 
 const ChatContent: React.FC = () => {
   return (
-    <div className="w-full">
+    <div className="w-full h-full flex flex-col">
       <ChatHeader />
+      <div className="flex-1 overflow-auto">{/* TODO: Messages Here */}</div>
+      <ChatTextBox />
     </div>
   );
 };
