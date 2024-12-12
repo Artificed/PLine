eb6f9ecf0626ec85fe109939ed5cb3af90c05874
artificed <paultsai.id@gmail.com>
2024-12-12 23:21:16 +0700

    feat: Full login layout & UI

---
+++ src/components/LoginForm.tsx
@@ -0,0 +1,39 @@
+import logo from "../assets/images/logos/line.svg";
+
+const LoginForm: React.FC = () => {
+  return (
+    <div className="w-[64%] border-r border-white/10 flex flex-col items-center text-white text-xs font-extralight">
+      <img src={logo} className="h-[4.1rem] w-[4.1rem] mb-4 mt-9" />
+      <div className="flex flex-col items-center">
+        <input
+          type="text"
+          className="bg-transparent border border-white/20 focus:outline-none w-[17rem] h-[2.7rem] rounded-t-[0.25rem] px-3"
+          placeholder="Email address"
+        />
+        <input
+          type="text"
+          className="bg-transparent border border-white/20 focus:outline-none w-[17rem] h-[2.7rem] -translate-y-[0.05rem] rounded-b-[0.25rem] px-3"
+          placeholder="Password"
+        />
+      </div>
+      <button
+        type="submit"
+        className="bg-white bg-opacity-65 w-[17rem] h-[2.6rem] my-[0.6rem] rounded-[0.25rem]"
+      >
+        Login
+      </button>
+      <div className="flex items-center justify-between w-[17rem]">
+        <div className="flex items-center">
+          <input type="checkbox" id="auto-login" className="mr-2" />
+          <label htmlFor="auto-login">Log me in automatically</label>
+        </div>
+        <a className="text-right">Reset password</a>
+      </div>
+      <div className="flex items-center justify-between w-[17rem] mb-6 mt-auto">
+        <a className="text-right">Log in with my smartphone {">"}</a>
+      </div>
+    </div>
+  );
+};
+
+export default LoginForm;
---
+++ src/components/QRLogin.tsx
@@ -0,0 +1,46 @@
+import login_qr from "../assets/images/defaults/login_qr.png";
+
+const QRLogin: React.FC = () => {
+  return (
+    <div className="w-[36%] flex flex-col items-center">
+      <div className="flex w-full justify-end gap-2 mr-6 translate-y-1">
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          fill="none"
+          viewBox="0 0 24 24"
+          strokeWidth={1.5}
+          stroke="gray"
+          className="size-5"
+        >
+          <path
+            strokeLinecap="round"
+            strokeLinejoin="round"
+            d="M9.879 7.519c1.171-1.025 3.071-1.025 4.242 0 1.172 1.025 1.172 2.687 0 3.712-.203.179-.43.326-.67.442-.745.361-1.45.999-1.45 1.827v.75M21 12a9 9 0 1 1-18 0 9 9 0 0 1 18 0Zm-9 5.25h.008v.008H12v-.008Z"
+          />
+        </svg>
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          fill="none"
+          viewBox="0 0 24 24"
+          strokeWidth={1.5}
+          stroke="gray"
+          className="size-5"
+        >
+          <path
+            strokeLinecap="round"
+            strokeLinejoin="round"
+            d="M3.75 6.75h16.5M3.75 12h16.5m-16.5 5.25h16.5"
+          />
+        </svg>
+      </div>
+      <img src={login_qr} className="mt-14" />
+      <p className="text-white text-[0.82rem] mt-4 mb-3">QR code login</p>
+      <p className="text-white text-opacity-50 text-xs font-extralight text-center w-36">
+        To scan your QR code, open LINE on your mobile device and tap the QR
+        code icon in the search box.
+      </p>
+    </div>
+  );
+};
+
+export default QRLogin;
